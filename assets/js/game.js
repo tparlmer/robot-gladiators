7f977698cae1432b415dd41158f51aa47cbea068
tparlmer <tparlmer@gmail.com>
2022-02-12 18:13:47 -0500

    3.1.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!");
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This log is a string, good for leaving yourself a message")
+// this iwll do math and log 20
+console.log(10 + 10)
+// what is this?
+console.log("Our robot's name is " + playerName)
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
